096be9638afc65ee168f439418b8b9788b2f4ea0
VVO89 <mister.vdowencko2012@yandex.ru>
2020-12-24 20:49:03 +0300

    lesson1

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -4,15 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // собираем команду
         Actions[] team = new Actions[3];
         team[0] = new Cat(3, 5, "Кот");
         team[1] = new Human(4,6, "Человек");
         team[2] = new Robot(10, 20, "Робот");
 
+        // собираем трассу
         Lets[] trace = new Lets[2];
         trace[0] = new Treadmill(5);
         trace[1] = new Wall(15);
 
+        // прохождение трассы
         for(int i = 0; i < team.length; ++i) {
             String name = team[i].getname();
 
