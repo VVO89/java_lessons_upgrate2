ce7a084d42ea6fce70221b3c4f5669e58793047c
VVO89 <mister.vdowencko2012@yandex.ru>
2021-01-17 13:07:11 +0300

    lesson5

--- src/lesson_flow/Main.java
+++ src/lesson_flow/Main.java
@@ -2,19 +2,27 @@ package lesson_flow;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
 
+    private static String PATH = "D://JAVA база/TEXT.txt";
+    private static String PATH_COPY = "D://JAVA база/TEXT_COPY.txt";
+
     public static void main(String[] args) {
 
-       /* int i;
-        FileInputStream fin = new FileInputStream(System.exit(););
-        String PATH = "D://JAVA база/TEXT.txt";
+        readFile();
+        copyFile();
+    }
+
+    private static void readFile() {
+
+        int i;
+        FileInputStream fin = null;
 
         try {
             fin = new FileInputStream(PATH);
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -28,7 +36,55 @@ public class Main {
             } while (i != -1);
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            //закрываем файл
+            try{
+                fin.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }*/
+    }
+
+    private static void copyFile() {
+
+        int i;
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+
+        // копирование файла
+        try {
+            // попытка открытия файла
+            fin = new FileInputStream(PATH);
+            fout = new FileOutputStream(PATH_COPY);
+
+            // читаем файл
+            do{
+                i = fin.read();
+                if (i!= -1){
+                    fout.write(i);
+                }
+            }while (i!=-1);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try{
+                if(fin!=null){
+                    fin.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Ошибка при закрытии входного файла");
+            }
+            try{
+                if(fout!=null){
+                    fout.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Ошибка при закрытии выходного файла");
+            }
+        }
     }
 }
